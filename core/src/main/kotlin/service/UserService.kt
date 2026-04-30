f25e30a1f618fe40b2daf99bee95673023850cc6
MareksCode <marekletsplay@yahoo.com>
2026-04-30 15:58:00 +0200

    user service

---
+++ core/src/main/kotlin/service/UserService.kt
@@ -0,0 +1,23 @@
+package service
+
+import model.User
+import repository.UserRepository
+
+class UserService(private val repository: UserRepository) {
+
+    fun createUser(user: User) = repository.create(user)
+
+    fun getUser(id: String): User? = repository.getById(id)
+
+    fun updateUser(user: User) = repository.update(user)
+
+    fun deleteUser(id: String) = repository.delete(id)
+
+    fun addFriend(userId: String, friendId: String) = repository.addFriend(userId, friendId)
+
+    fun removeFriend(userId: String, friendId: String) = repository.removeFriend(userId, friendId)
+
+    fun getFriends(userId: String): List<User> = repository.getFriends(userId)
+
+    fun suggestFriends(userId: String): List<User> = repository.getFriendsOfFriends(userId)
+}
